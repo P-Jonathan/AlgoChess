9b47e6a62737022c341dccbea5b7ada0aa3b7ae3
John <JonathanNicolasPaz@Outlook.com>
2019-11-02 14:21:54 -0300

    Update UnidadDeInfanteria && Unidad

--- src/main/java/fiuba/algo3/AlgoChess/Unidad.java
+++ src/main/java/fiuba/algo3/AlgoChess/Unidad.java
@@ -1,6 +1,8 @@
 package fiuba.algo3.AlgoChess;
 
 public abstract class Unidad {
+	int vida;
 	public abstract void recibirDanio(int danio);
 	public abstract void aumentarVida(int vida);
+	public abstract int getVida();
 }
--- src/main/java/fiuba/algo3/AlgoChess/UnidadDeInfanteria.java
+++ src/main/java/fiuba/algo3/AlgoChess/UnidadDeInfanteria.java
@@ -1,17 +1,23 @@
 package fiuba.algo3.AlgoChess;
 
-public class UnidadDeInfanteria {
-	private int vida;
+public class UnidadDeInfanteria extends Unidad {
 	
 	public UnidadDeInfanteria() {
-		this.vida = 100;
+		vida = 100;
 	}
 	
+	@Override
 	public int getVida() {
-		return this.vida;
+		return vida;
 	}
 	
+	@Override
 	public void recibirDanio(int danio) {
-		this.vida -= danio;
+		vida -= danio;
+	}
+
+	@Override
+	public void aumentarVida(int vida) {
+		this.vida += vida;
 	}
 }
