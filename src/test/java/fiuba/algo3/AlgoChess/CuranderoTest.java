871e813244ebbb5747e83f19f54ec443c5cb9cec
John <JonathanNicolasPaz@Outlook.com>
2019-11-04 17:59:01 -0300

    Se actualizo el test del curandero para que funcione con los nuevo requerimientos.

--- src/test/java/fiuba/algo3/AlgoChess/CuranderoTest.java
+++ src/test/java/fiuba/algo3/AlgoChess/CuranderoTest.java
@@ -2,41 +2,42 @@ package fiuba.algo3.AlgoChess;
 
 import org.junit.jupiter.api.Test;
 
+import fiuba.algo3.AlgoChess.unidades.Curandero;
+import fiuba.algo3.AlgoChess.unidades.UnidadDeInfanteria;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CuranderoTest {
-    @Test
-    public void creoUnidadCUranderoYAlQuitarle50DeVidaLeQuedan25() {
-        Curandero curandero = new Curandero();
-        assertEquals(curandero.getVida(), 75);
-        curandero.recibirDanio(50);
-        assertEquals(curandero.getVida(), 25);
-    }
+	@Test
+	public void creoUnidadCUranderoYAlQuitarle50DeVidaLeQuedan25() {
+		Curandero curandero = new Curandero("Test", new Tablero(), new Posicion2D(0, 0));
+		assertEquals(curandero.getVida(), 75);
+		curandero.recibirDanio(50);
+		assertEquals(curandero.getVida(), 25);
+	}
 
-    @Test
-    void creoUnidadCUranderoLeAumentoLaVidaEn20YSuVidaEs95() {
-        Curandero curandero = new Curandero();
-        assertEquals(curandero.getVida(), 75);
-        curandero.aumentarVida(20);
-        assertEquals(curandero.getVida(), 95);
-    }
+	@Test
+	void creoUnidadCUranderoLeAumentoLaVidaEn20YSuVidaEs95() {
+		Curandero curandero = new Curandero("Test", new Tablero(), new Posicion2D(0, 0));
+		assertEquals(curandero.getVida(), 75);
+		curandero.aumentarVida(20);
+		assertEquals(curandero.getVida(), 95);
+	}
 
-    @Test
-    void creoCUranderoLeQuito25DeVidaYSuVidaEs50() {
-        Curandero curandero = new Curandero();
-        assertEquals(curandero.getVida(), 75);
-        curandero.recibirDanio(25);
-        assertEquals(curandero.getVida(), 50);
-    }
+	@Test
+	void creoCUranderoLeQuito25DeVidaYSuVidaEs50() {
+		Curandero curandero = new Curandero("Test", new Tablero(), new Posicion2D(0, 0));
+		assertEquals(curandero.getVida(), 75);
+		curandero.recibirDanio(25);
+		assertEquals(curandero.getVida(), 50);
+	}
 
-
-    @Test
-    public void unCuranderoCuraAUnaUNidadDeInfanteriaYSuVidaDeberiaAumentarEn15() {
-        Curandero curandero = new Curandero();
-        UnidadDeInfanteria soldado = new UnidadDeInfanteria();
-        assertEquals(soldado.getVida(), 100);
-        curandero.curar(soldado);
-        assertEquals(soldado.getVida(), 115);
-    }
-
+	@Test
+	public void unCuranderoCuraAUnaUNidadDeInfanteriaYSuVidaDeberiaAumentarEn15() {
+		Curandero curandero = new Curandero("Test", new Tablero(), new Posicion2D(0, 0));
+		UnidadDeInfanteria soldado = new UnidadDeInfanteria("Test", new Tablero(), new Posicion2D(1, 1));
+		assertEquals(soldado.getVida(), 100);
+		curandero.curar(soldado);
+		assertEquals(soldado.getVida(), 115);
+	}
 }
