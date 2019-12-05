de93696b3aeba354e6e2087ae391428d36314926
andresguasta <andresguasta@hotmail.com>
2019-12-05 02:17:07 -0300

    Agrego la cara de las nuevas unidades al shop

--- src/main/java/fiuba/algo3/algochess/vista/comprabutton/CompraDeCatapultaView.java
+++ src/main/java/fiuba/algo3/algochess/vista/comprabutton/CompraDeCatapultaView.java
@@ -10,7 +10,8 @@ import javafx.scene.control.Label;
 public class CompraDeCatapultaView extends CompraDeUnidadView {
     public CompraDeCatapultaView(Tablero tablero, TableroView tableroView, ShopView shopView) {
         super(new ComprarCatapultaHandler(tablero, tableroView, shopView), tableroView,
-                new Sprite("src/main/resources/caras.png", 6, 0, 27, 19, 50, 50)
+                new Sprite("src/main/resources/caras.png", 6, 0, 27, 19, 50, 50),
+                new Sprite("src/main/resources/caras.png", 7, 0, 27, 19, 50, 50)
         );
         this.setText("Catapulta\nCosto(5)\nAtaque a distancia larga(20)");
         this.setWrapText(false);
--- src/main/java/fiuba/algo3/algochess/vista/comprabutton/CompraDeUnidadView.java
+++ src/main/java/fiuba/algo3/algochess/vista/comprabutton/CompraDeUnidadView.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.TextAlignment;
 
@@ -17,24 +18,30 @@ public abstract class CompraDeUnidadView extends Button {
 
     protected CompraDeUnidadController cuc;
     protected TableroView tableroView;
-    protected Sprite sprite;
+    protected Sprite spriteA;
+    protected Sprite spriteB;
 
-    public CompraDeUnidadView(CompraDeUnidadController cuc, TableroView tableroView, Sprite sprite) {
+    public CompraDeUnidadView(CompraDeUnidadController cuc, TableroView tableroView, Sprite spriteA, Sprite spriteB) {
         this.tableroView = tableroView;
-        this.sprite = sprite;
+        this.spriteA = spriteA;
+        this.spriteB = spriteB;
         this.cuc = cuc;
 
         this.setMinSize(DEFAULT_BUTTON_WIDTH, DEFAULT_BUTTON_HEIGHT);
         this.setMaxSize(DEFAULT_BUTTON_WIDTH, DEFAULT_BUTTON_HEIGHT);
         this.setTextAlignment(TextAlignment.CENTER);
-        this.setGraphic(getImageView());
-        this.setText("Buy Button");
-        this.setWrapText(true);
+
+        HBox hb = new HBox();
+        hb.getChildren().add(getImageViewA());
+        hb.getChildren().add(getImageViewB());
+
+        this.setGraphic(hb);
+
         this.setOnAction(cuc);
     }
 
-    private ImageView getImageView() {
-        Image img = getImage();
+    private ImageView getImageViewA() {
+        Image img = getImageA();
         ImageView imageView = new ImageView(img);
         imageView.setFitHeight(img.getHeight());
         imageView.setFitWidth(img.getWidth());
@@ -42,7 +49,20 @@ public abstract class CompraDeUnidadView extends Button {
         return imageView;
     }
 
-    private Image getImage() {
-        return SwingFXUtils.toFXImage(sprite.getActualFrame(), null) ;
+    private ImageView getImageViewB() {
+        Image img = getImageB();
+        ImageView imageView = new ImageView(img);
+        imageView.setFitHeight(img.getHeight());
+        imageView.setFitWidth(img.getWidth());
+
+        return imageView;
+    }
+
+    private Image getImageA() {
+        return SwingFXUtils.toFXImage(spriteA.getActualFrame(), null) ;
+    }
+
+    private Image getImageB() {
+        return SwingFXUtils.toFXImage(spriteB.getActualFrame(), null) ;
     }
 }
